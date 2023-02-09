7ce87843c6b33f9be581892b2fd680762fed972d
unohayato <mamidori1p1p@gmail.com>
2023-02-09 23:01:30 +0900

    feat: :bulb:  add application name tips

--- app/apps.py
+++ app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "app"
+    verbose_name = "アプリ" #画面に表示されるアプリケーション名の変更はここでできる
