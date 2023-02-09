318bc2b557fe62f3dfa1ac0584454e7a080af8d0
unohayato <mamidori1p1p@gmail.com>
2023-02-09 22:30:13 +0900

    feat: :sparkles: add sample model on amdin

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+  class Meta:
+    db_table = 'book' #テーブル名の変更、指定
+    verbose_name = '本' #モデル単数の場合の名前
+    verbose_name_plural = '本たち' #モデル複数の場合の名前
+    
+  title = models.CharField(verbose_name='タイトル', max_length=255)
+  
+  # ポストしたモデル一つ一つの管理サイト上での表示名
+  def __str__(self):
+    return self.title
