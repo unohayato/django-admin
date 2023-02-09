eae726cdd157454e787652a391cec77e2cf20b41
unohayato <mamidori1p1p@gmail.com>
2023-02-09 23:44:24 +0900

    feat: :sparkles: add AdminSite and ModelAdmin

--- app/admin.py
+++ app/admin.py
@@ -2,4 +2,20 @@ from django.contrib import admin
 from .models import Book
 # Register your models here.
 
-admin.site.register(Book)
+#ここでモデル(管理画面上の)ごとの制限などをかけたい場合はModelAdminをオーバーライドした独自のクラスを定義して以下のAdminSiteクラスのregisterメソッドの第二引数に渡す
+
+
+"""
+デコレータを使わない場合
+
+class BookAdmin(admin.ModelAdmin): 
+  pass #何もしない場合
+
+admin.site.register(Book, BookAdmin)
+"""
+
+
+#デコレータを使う場合
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+  pass
